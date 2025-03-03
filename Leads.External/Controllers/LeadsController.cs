f471f3696dfb722d4ceb84bd98b97144ad8af8ca
Katlego Rathelele <Katlego8560>
2025-03-03 20:58:48 +0200

    New Sprint changes

---
+++ Leads.External/Controllers/LeadsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leads.External.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeadsController : ControllerBase
+    {
+        private readonly string baseUrl = "https://localhost:7060/api/leads/";
+
+        [HttpGet]
+        public IActionResult GetLead([FromQuery] Guid id)
+        {
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+           // var response = client.GetAsync("?" + id);
+
+            return Ok();
+        }
+    }
+}
