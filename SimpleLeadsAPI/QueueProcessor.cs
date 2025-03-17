d06ac28623641476a1e3c39912e9332963ff6a1b
Katlego Rathelele <Katlego8560>
2025-03-17 17:22:46 +0200

    Saving lead into database

--- SimpleLeadsAPI/QueueProcessor.cs
+++ SimpleLeadsAPI/QueueProcessor.cs
@@ -1,13 +1,21 @@
 ﻿using EasyNetQ;
 using Messages;
+using SimpleLeadsAPI.Controllers;
 using SimpleLeadsAPI.Models;
 using SimpleLeadsAPI.Services;
 
 namespace SimpleLeadsAPI
 {
-    public class QueueProcessor(IServiceProvider serviceProvider) : BackgroundService
+    public class QueueProcessor : BackgroundService
     {
-        private readonly IServiceProvider _ServiceProvider = serviceProvider;
+        private readonly IServiceProvider _ServiceProvider;
+        private readonly ApplicationDbContext _Context;
+
+        public QueueProcessor(IServiceProvider serviceProvider, ApplicationDbContext context)
+        {
+            _ServiceProvider = serviceProvider;
+            _Context = context;
+        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -15,14 +23,14 @@ namespace SimpleLeadsAPI
             IBus bus = scope.ServiceProvider.GetRequiredService<IBus>();
 
             ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            await bus.PubSub.SubscribeAsync<LeadMessage>("new-lead", HandleNewLeadAsync);
-        }
+            await bus.PubSub.SubscribeAsync<LeadMessage>("new-lead", message => {
+                Console.WriteLine("Lead received");
+                Console.WriteLine(message.ContactNumber);
+                Console.WriteLine(message.FullName);
+            });
 
-        private void HandleNewLeadAsync(LeadMessage dto)
-        {
-            Console.WriteLine("Lead received");
-            Console.WriteLine(dto.ContactNumber);
-            Console.WriteLine(dto.FullName);
+            _Context.Leads.Add(new Lead()); 
+            _Context.SaveChanges();
         }
     }
 }
